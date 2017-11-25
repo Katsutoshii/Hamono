62136586b57731f6b10bbd7f38f73b5321350cfe
Katsutoshii <Josiah.K.Putman.20@Dartmouth.edu>
2017-11-25 18:52:05 -0500

    Added in code support for animations Added jump counter, now limited to 2 jumps

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,34 +8,33 @@ public class Player : MonoBehaviour {
 	public float maxSpeed = 4f;
 	public float jumpPower = 300f;
 	public float kp = 100f;
+	public int jumps = 2;
 
 	public bool grounded;
 	public bool autoPathing;
 
 	public Vector2 targetA;
 	private Rigidbody2D rb;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Start");
 		rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
 		autoPathing = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if(Input.GetKeyDown(key:KeyCode.W)) {
-			rb.AddForce(Vector2.up * jumpPower);
-			autoPathing = false;
-		}
-
 		// turn the sprite around
 		if(rb.velocity.x > 0.1f) 
 			transform.localScale = new Vector3(1, 1, 1);
 		else if(rb.velocity.x < -0.1f) 
 			transform.localScale = new Vector3(-1, 1, 1);
 
+        anim.SetBool("grounded", grounded);
+        anim.SetFloat("speed", Mathf.Abs(rb.velocity.x));
 	}
 
 	/// <summary>
@@ -53,20 +52,29 @@ public class Player : MonoBehaviour {
 			autoPathing = false;
 		}
 
-		// if we clicked, start autopathing towards that direction
-		if(Input.GetMouseButtonDown(0)) { // if left click
-			autoPathing = true;
-			Vector3 v3 = Input.mousePosition;
-			// v3.z = 10.0;
-			v3 = Camera.main.ScreenToWorldPoint(v3);
+        // for jumping
+        if (Input.GetKeyDown(key: KeyCode.W) && jumps > 0)
+        {
+            rb.velocity.Set(rb.velocity.x, 0); // prevents stacking velocity
+            rb.Sleep();
+            rb.AddForce(Vector2.up * jumpPower);
+            jumps--;
+            autoPathing = false;
+        }
 
-			targetA = new Vector2(v3.x, v3.y);
+        // if we clicked, start autopathing towards that direction
+        if (Input.GetMouseButtonDown(0)) { // if left click
+            autoPathing = true;
+
+            Vector3 worldSpaceClickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetA = new Vector2(worldSpaceClickPosition.x, worldSpaceClickPosition.y);
+         
 		}
 
-		if(autoPathing) {
+		if (autoPathing) {
 			float error = targetA.x - transform.position.x;
 
-			if(Mathf.Abs(error) > 1) {
+			if (Mathf.Abs(error) > 2) {
 				rb.AddForce(Vector2.left  * -1 * error * kp);
 			}
 			else {
@@ -75,16 +83,16 @@ public class Player : MonoBehaviour {
 		}
 
 		// limit the speed
-		if(rb.velocity.x > maxSpeed) {
+		if (rb.velocity.x > maxSpeed) {
 			rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
 		}
-		else if(rb.velocity.x < -maxSpeed) {
+		else if (rb.velocity.x < -maxSpeed) {
 			rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
 		}
 	}
 
     void OnGUI()
     {
-        GUI.Box(new Rect(100, 100, 150, 150), rb.velocity.ToString());
+        GUI.Label(new Rect(20, 20, 100, 100), "Player velocity = " + rb.velocity.ToString());
     }
 }
