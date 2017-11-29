7a2c12e5acf7d6010a11f309365ef035bcaeb5cf
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-11-29 12:19:52 -0800

    Created new file

---
+++ Assets/Scripts/OptimalPath.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptimalPath : MonoBehaviour {
+
+  void Start() {
+      Debug.Log("waddup pimps");
+  }
+
+  void Update() {
+
+  }
+
+}
