577059047699d8c5353267f4a8665b147371deb4
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-11-30 15:02:27 -0800

    Moving file out of Player.cs

--- Assets/Scripts/OptimalPath.cs
+++ Assets/Scripts/OptimalPath.cs
@@ -5,252 +5,257 @@ using UnityEngine;
 
 public class OptimalPath : MonoBehaviour {
 
-  // Graph space;
-  // public Vector2 targetA;
+  Graph space;
+  public Stack<Vector2> reversedAutoPath;
+  public Vector2 targetA;
 
-  // private class Graph {
-  //   HashSet<Vertex> vertices = new HashSet<Vertex>();
-  //   Dictionary<Vertex, HashSet<Vertex>> edges = new Dictionary<Vertex, HashSet<Vertex>>();
+  void Start() {
+    space = new Graph();
+		reversedAutoPath = new Stack<Vector2>();
+  }
 
-  //   public Graph() {}
+  	// AutoPathing Script
+	private class Graph {
+    public HashSet<Vertex> vertices = new HashSet<Vertex>();
+    public Dictionary<Vertex, HashSet<Vertex>> edges = new Dictionary<Vertex, HashSet<Vertex>>();
 
-  //   public void addEdge(Vertex pointA, Vertex pointB) {
-  //     if (!edges.ContainsKey(pointA)) {
-  //       HashSet<Vertex> list = new HashSet<Vertex>();
-  //       edges[pointA] = list;
-  //     }
-  //     if (!edges.ContainsKey(pointB)) {
-  //       HashSet<Vertex> list = new HashSet<Vertex>();
-  //       edges[pointB] = list;
-  //     }
-  //     HashSet<Vertex> listA = edges[pointA];
-  //     HashSet<Vertex> listB = edges[pointB];
-  //     listA.Add(pointB);
-  //     listB.Add(pointA);
-  //     edges[pointA] = listA;
-  //     edges[pointB] = listB;
-  //   }
+    public Graph() {}
 
-  //   public void addVertex(Vertex point) {
-  //     vertices.Add(point);
-  //   }
+    public void addEdge(Vertex pointA, Vertex pointB) {
+      if (!edges.ContainsKey(pointA)) {
+        HashSet<Vertex> list = new HashSet<Vertex>();
+        edges[pointA] = list;
+      }
+      if (!edges.ContainsKey(pointB)) {
+        HashSet<Vertex> list = new HashSet<Vertex>();
+        edges[pointB] = list;
+      }
+      HashSet<Vertex> listA = edges[pointA];
+      HashSet<Vertex> listB = edges[pointB];
+      listA.Add(pointB);
+      listB.Add(pointA);
+      edges[pointA] = listA;
+      edges[pointB] = listB;
+    }
 
-  //   public HashSet<Vertex> getEdges(Vertex point) {
-  //     HashSet<Vertex> output = null;
-  //     this.edges.TryGetValue(point, out output);
-  //     return output;
-  //   }
+    public void addVertex(Vertex point) {
+      vertices.Add(point);
+    }
 
-  //   public Vertex getVertex(Vector2 data) {
-  //     foreach(Vertex item in this.vertices) {
-  //       if (item.data == data) {
-  //         return item;
-  //       }
-  //     }
-  //     return null;
-  //   }
+    public HashSet<Vertex> getEdges(Vertex point) {
+      HashSet<Vertex> output = null;
+      this.edges.TryGetValue(point, out output);
+      return output;
+    }
 
-  //   public void populateEdges() {
-  //     Debug.Log(this.vertices.Count);
-  //     foreach(Vertex item in this.vertices) {
-  //       foreach(Vertex compareItem in this.vertices) {
-  //         if (item != compareItem) {
-  //           float distance = Vector2.Distance(item.data, compareItem.data);
-  //           if (distance <= .5f) {
-  //             this.addEdge(item, compareItem);
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+    public Vertex getVertex(Vector2 data) {
+      foreach(Vertex item in this.vertices) {
+        if (item.data == data) {
+          return item;
+        }
+      }
+      return null;
+    }
 
-  // private class Vertex : IComparable<Vertex> {
-  //   public Vector2 data;
-  //   public float priority;
+    public void populateEdges() {
+      Debug.Log(this.vertices.Count);
+      foreach(Vertex item in this.vertices) {
+        foreach(Vertex compareItem in this.vertices) {
+          if (item != compareItem) {
+            float distance = Vector2.Distance(item.data, compareItem.data);
+            if (distance <= .5f) {
+              this.addEdge(item, compareItem);
+            }
+          }
+        }
+      }
+    }
+  }
 
-  //   public Vertex(Vector2 data) {
-  //     this.data = data;
-  //   }
+  private class Vertex : IComparable<Vertex> {
+    public Vector2 data;
+    public float priority;
 
-  //   public int CompareTo(Vertex other) {
-  //     if (other == null) {
-  //       return -1;
-  //     }
-  //     if (this.priority == other.priority) return 0;
-  //     else if (this.priority > other.priority) return -1;
-  //     return 1;
-  //   }
-  // }
+    public Vertex(Vector2 data) {
+      this.data = data;
+    }
 
-  // private class PriorityQueue <T> where T : IComparable <T> {
-  //   private List <T> data;
+    public int CompareTo(Vertex other) {
+      if (other == null) {
+        return -1;
+      }
+      if (this.priority == other.priority) return 0;
+      else if (this.priority > other.priority) return 1;
+      return -1;
+    }
+  }
 
-  //   public PriorityQueue()
-  //   {
-  //     this.data = new List <T>();
-  //   }
+  private class PriorityQueue <T> where T : IComparable <T> {
+    private List <T> data;
 
-  //   public void Enqueue(T item) {
-  //     data.Add(item);
-  //     int ci = data.Count - 1;
-  //     while (ci  > 0)
-  //     {
-  //       int pi = (ci - 1) / 2;
-  //       if (data[ci].CompareTo(data[pi]) >= 0)
-  //         break;
-  //       T tmp = data[ci]; data[ci] = data[pi]; data[pi] = tmp;
-  //       ci = pi;
-  //     }
-  //   }
+    public PriorityQueue()
+    {
+      this.data = new List <T>();
+    }
 
-  //   public T Dequeue() {
-  //     // Assumes pq isn't empty
-  //     int li = data.Count - 1;
-  //     T frontItem = data[0];
-  //     data[0] = data[li];
-  //     data.RemoveAt(li);
+    public void Enqueue(T item) {
+      data.Add(item);
+      int ci = data.Count - 1;
+      while (ci  > 0)
+      {
+        int pi = (ci - 1) / 2;
+        if (data[ci].CompareTo(data[pi]) >= 0)
+          break;
+        T tmp = data[ci]; data[ci] = data[pi]; data[pi] = tmp;
+        ci = pi;
+      }
+    }
 
-  //     --li;
-  //     int pi = 0;
-  //     while (true)
-  //     {
-  //       int ci = pi * 2 + 1;
-  //       if (ci  > li) break;
-  //       int rc = ci + 1;
-  //       if (rc  <= li && data[rc].CompareTo(data[ci])  < 0)
-  //         ci = rc;
-  //       if (data[pi].CompareTo(data[ci])  <= 0) break;
-  //       T tmp = data[pi]; data[pi] = data[ci]; data[ci] = tmp;
-  //       pi = ci;
-  //     }
-  //     return frontItem;
-  //   }
+    public T Dequeue() {
+      // Assumes pq isn't empty
+      int li = data.Count - 1;
+      T frontItem = data[0];
+      data[0] = data[li];
+      data.RemoveAt(li);
 
-  //   public int Count() {
-  //     return data.Count;
-  //   }
-  // }
+      --li;
+      int pi = 0;
+      while (true)
+      {
+        int ci = pi * 2 + 1;
+        if (ci  > li) break;
+        int rc = ci + 1;
+        if (rc  <= li && data[rc].CompareTo(data[ci])  < 0)
+          ci = rc;
+        if (data[pi].CompareTo(data[ci])  <= 0) break;
+        T tmp = data[pi]; data[pi] = data[ci]; data[ci] = tmp;
+        pi = ci;
+      }
+      return frontItem;
+    }
 
-  // void Start() {
-  // }
+    public int Count() {
+      return data.Count;
+    }
+  }
 
-  // void FixedUpdate() {
-  //   if (Input.GetMouseButtonDown(0)) {
-  //     targetA = MouseWorldPosition2D();
-  //     GenerateGraph(targetA);
-  //   }
-  // }
+  public Stack<Vector2> GenerateGraph(Vector2 targetA) {
+    space = new Graph();
+    float xDist = targetA.x - transform.position.x;
+    float yDist = targetA.y - transform.position.y;
+    float currentX = transform.position.x;
+    float startingY = transform.position.y;
+    if (xDist > 0) {
+      while (targetA.x - currentX > .2f) {
+        float currentY = startingY;
+        if (yDist > 0) {
+          while (3f - currentY > .2f) {
+            Vertex point = new Vertex(new Vector2(currentX, currentY));
+						if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(point.data)) {
+							space.addVertex(point);
+						}
+            currentY = currentY + .2f;
+          }
+        } else {
+          while (3f - currentY < .2f) {
+            Vertex point = new Vertex(new Vector2(currentX, currentY));
+						if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(point.data)) {
+							space.addVertex(point);
+						}
+            currentY = currentY - .2f;
+          }
+        }
+        currentX = currentX + .2f;
+      }
+    } else {
+      while (targetA.x - currentX < .2f) {
+        float currentY = startingY;
+        if (yDist > 0) {
+          while (3f - currentY > .2f) {
+						Vertex point = new Vertex(new Vector2(currentX, currentY));
+						if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(point.data)) {
+							space.addVertex(point);
+						}
+            currentY = currentY + .2f;
+          }
+        } else {
+          while (3f - currentY < .2f) {
+						Vertex point = new Vertex(new Vector2(currentX, currentY));
+						if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(point.data)) {
+							space.addVertex(point);
+						}
+            currentY = currentY - .2f;
+          }
+        }
+        currentX = currentX - .2f;
+      }
+    }
+		Vertex dest = new Vertex(targetA);
+		if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(dest.data)) {
+			space.addVertex(dest);
+		}
+    space.populateEdges();
+		Debug.Log(space.getVertex(new Vector2(transform.position.x, transform.position.y)));
+  	Stack<Vector2> list = AStar(space.getVertex(new Vector2(transform.position.x, transform.position.y)), space.getVertex(targetA));
+		return list;
+  }
 
-  // private void GenerateGraph(Vector2 targetA) {
-  //   space = new Graph();
-  //   float xDist = targetA.x - transform.position.x;
-  //   float yDist = targetA.y - transform.position.y;
-  //   float currentX = transform.position.x;
-  //   float startingY = transform.position.y;
-  //   if (xDist > 0) {
-  //     while (targetA.x - currentX > .2f) {
-  //       float currentY = startingY;
-  //       if (yDist > 0) {
-  //         while (targetA.y - currentY > .2f) {
-  //           Vertex point = new Vertex(new Vector2(currentX, currentY));
-  //           space.addVertex(point);
-  //           currentY = currentY + .2f;
-  //         }
-  //       } else {
-  //         while (targetA.y - currentY < .2f) {
-  //           Vertex point = new Vertex(new Vector2(currentX, currentY));
-  //           space.addVertex(point);
-  //           currentY = currentY - .2f;
-  //         }
-  //       }
-  //       currentX = currentX + .2f;
-  //     }
-  //   } else {
-  //     while (targetA.x - currentX < .2f) {
-  //       float currentY = startingY;
-  //       if (yDist > 0) {
-  //         while (targetA.y - currentY > .2f) {
-  //           Vertex point = new Vertex(new Vector2(currentX, currentY));
-  //           space.addVertex(point);
-  //           currentY = currentY + .2f;
-  //         }
-  //       } else {
-  //         while (targetA.y - currentY < .2f) {
-  //           Vertex point = new Vertex(new Vector2(currentX, currentY));
-  //           space.addVertex(point);
-  //           currentY = currentY - .2f;
-  //         }
-  //       }
-  //       currentX = currentX - .2f;
-  //     }
-  //   }
-  //   space.addVertex(new Vertex(new Vector2(targetA.x, targetA.y)));
-  //   space.populateEdges();
-  //   Dictionary<Vertex, Vertex> list = a_star(space.getVertex(new Vector2(transform.position.x, transform.position.y)), space.getVertex(targetA));
-  // }
+  private Stack<Vector2> backchain(Dictionary<Vertex, Vertex> path, Vertex destination) {
+    string pathString = "";
+    pathString = pathString + destination.data + " -> ";
+    Vertex current = path[destination];
+    int count = 0;
+		Stack<Vector2> reversedPath = new Stack<Vector2>();
+		reversedPath.Push(destination.data);
 
-  // private void backchain(Dictionary<Vertex, Vertex> path, Vertex destination) {
-  //   string pathString = "";
-  //   pathString = pathString + destination.data + " -> ";
-  //   Vertex current = path[destination];
-  //   int count = 0;
+    while (current != null) {
+      count++;
+      pathString = pathString + current.data + " -> ";
+			reversedPath.Push(current.data);
+      current = path[current];
+    }
+    // Debug.Log("path: " + pathString);
+    // Debug.Log("count: " + count);
+		return reversedPath;
+  }
 
-  //   while (current != null) {
-  //     count++;
-  //     pathString = pathString + current.data + " -> ";
-  //     current = path[current];
-  //   }
-  //   Debug.Log("path: " + pathString);
-  //   Debug.Log("count: " + count);
-  // }
+	private float heuristic(Vertex a, Vertex b) {
+		return Vector2.Distance(a.data, b.data);
+  }
 
-  // // AStar Seach Algorithm
-  // private Dictionary<Vertex, Vertex> a_star(Vertex start, Vertex dest) {
+  // AStar Seach Algorithm
+  private Stack<Vector2> AStar(Vertex start, Vertex dest) {
 
-  //   Debug.Log("Start: " + start.data);
-  //   Debug.Log("Dest: " + dest.data);
+		if (start == null || dest == null) return new Stack<Vector2>();
 
-  //   // Queue<Vertex> frontier = new Queue<Vertex>();
-  //   PriorityQueue<Vertex> frontier = new PriorityQueue<Vertex>();
-  //   start.priority = 0;
-  //   frontier.Enqueue(start);
-  //   Dictionary<Vertex, Vertex> cameFrom = new Dictionary<Vertex, Vertex>();
-  //   Dictionary<Vertex, float> costSoFar = new Dictionary<Vertex, float>();
-  //   cameFrom[start] = null;
-  //   costSoFar[start] = 0;
+		Debug.Log("Start: " + start.data);
+    Debug.Log("Dest: " + dest.data);
 
-  //   while (frontier.Count() > 0) {
-  //     Vertex current = frontier.Dequeue();
-  //     if (current.data == dest.data) {
-  //       Debug.Log("found the solution");
-  //       backchain(cameFrom, dest);
-  //       break;
-  //     }
-  //     foreach (Vertex next in space.getEdges(current)) {
-  //       float newCost = costSoFar[current] + Vector2.Distance(current.data, next.data);
-  //       if (!costSoFar.ContainsKey(next) || newCost < costSoFar[next]) {
-  //         costSoFar[next] = newCost;
-  //         next.priority = newCost + heuristic(dest, next);
-  //         frontier.Enqueue(next);
-  //         cameFrom[next] = current;
-  //       }
-  //     }
-  //   }
-  //   return cameFrom;
-    
-  // }
+    // Queue<Vertex> frontier = new Queue<Vertex>();
+    PriorityQueue<Vertex> frontier = new PriorityQueue<Vertex>();
+    start.priority = 0;
+    frontier.Enqueue(start);
+    Dictionary<Vertex, Vertex> cameFrom = new Dictionary<Vertex, Vertex>();
+    Dictionary<Vertex, float> costSoFar = new Dictionary<Vertex, float>();
+    cameFrom[start] = null;
+    costSoFar[start] = 0;
 
-  // private float heuristic(Vertex a, Vertex b) {
-  //  float dx = Mathf.Abs(a.data.x - b.data.x);
-  //  float dy = Mathf.Abs(a.data.y - b.data.y);
-  // //  return Mathf.Sqrt(dx * dx + dy * dy);
-  // return dx + dy;
-  // }
-
-  // private Vector2 MouseWorldPosition2D() {
-	// 	Vector3 worldSpaceClickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-	// 	return new Vector2(worldSpaceClickPosition.x, worldSpaceClickPosition.y);
-	// }
-
+    while (frontier.Count() > 0) {
+      Vertex current = frontier.Dequeue();
+      if (current.data == dest.data) {
+        Debug.Log("found the solution");
+        return backchain(cameFrom, dest);
+      }
+      foreach (Vertex next in space.getEdges(current)) {
+        float newCost = costSoFar[current] + Vector2.Distance(current.data, next.data);
+        if (!costSoFar.ContainsKey(next) || newCost < costSoFar[next]) {
+          costSoFar[next] = newCost;
+          next.priority = newCost + heuristic(dest, next);
+          frontier.Enqueue(next);
+          cameFrom[next] = current;
+        }
+      }
+    }
+    return new Stack<Vector2>();
+  }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,8 +10,10 @@ public class Player : MonoBehaviour {
 	public float jumpPower;
 	public int jumps;
 
-	Graph space;
-	Stack<Vector2> reversedAutoPath;
+	public OptimalPath optimalPath;
+
+	// Graph space;
+	// Stack<Vector2> reversedAutoPath;
 
 	public enum State {
 		idle,
@@ -58,11 +60,10 @@ public class Player : MonoBehaviour {
 	void Start () {
 		Debug.Log("Start");
 		rb = gameObject.GetComponent<Rigidbody2D>();
-        anim = gameObject.GetComponent<Animator>();
+    anim = gameObject.GetComponent<Animator>();
+		optimalPath = gameObject.GetComponent<OptimalPath>();
 		state = State.idle;
 		completedAutoPathing = false;
-		space = new Graph();
-		reversedAutoPath = new Stack<Vector2>();
 	}
 	
 	// Update is called once per frame
@@ -118,8 +119,7 @@ public class Player : MonoBehaviour {
             state = State.autoPathing;
 						completedAutoPathing = false;
             targetA = MouseWorldPosition2D();
-						reversedAutoPath = GenerateGraph(targetA);
-						// Debug.Log("fixing this right now: " + Physics2D.OverlapPoint(targetA).gameObject.layer);
+						optimalPath.GenerateGraph(targetA);
 			// turn the sprite around
 			if (targetA.x > transform.position.x)
 				transform.localScale = new Vector3(1, 1, 1);
@@ -192,7 +192,7 @@ public class Player : MonoBehaviour {
 	/// <param name="type"> label that tells function if it has a successor action </param>
 
 	private int AutoPath(string type) {
-		if (reversedAutoPath.Count == 0) {
+		if (optimalPath.reversedAutoPath.Count == 0) {
 			rb.gravityScale = 0;
 			rb.velocity = new Vector3(0, 0);
 			if (type == "none") state = State.idle;
@@ -200,7 +200,7 @@ public class Player : MonoBehaviour {
 			return 1;
 		}
 
-		Vector2 nextLocation = reversedAutoPath.Pop();
+		Vector2 nextLocation = optimalPath.reversedAutoPath.Pop();
 
 		float xDist = nextLocation.x - transform.position.x;
 		float yDist = nextLocation.y - transform.position.y;
@@ -322,263 +322,4 @@ public class Player : MonoBehaviour {
 			else slashType = SlashType.upSlash;
 		}
 	}
-
-
-	// AutoPathing Script
-
-	private class Graph {
-    public HashSet<Vertex> vertices = new HashSet<Vertex>();
-    public Dictionary<Vertex, HashSet<Vertex>> edges = new Dictionary<Vertex, HashSet<Vertex>>();
-
-    public Graph() {}
-
-    public void addEdge(Vertex pointA, Vertex pointB) {
-      if (!edges.ContainsKey(pointA)) {
-        HashSet<Vertex> list = new HashSet<Vertex>();
-        edges[pointA] = list;
-      }
-      if (!edges.ContainsKey(pointB)) {
-        HashSet<Vertex> list = new HashSet<Vertex>();
-        edges[pointB] = list;
-      }
-      HashSet<Vertex> listA = edges[pointA];
-      HashSet<Vertex> listB = edges[pointB];
-      listA.Add(pointB);
-      listB.Add(pointA);
-      edges[pointA] = listA;
-      edges[pointB] = listB;
-    }
-
-    public void addVertex(Vertex point) {
-      vertices.Add(point);
-    }
-
-    public HashSet<Vertex> getEdges(Vertex point) {
-      HashSet<Vertex> output = null;
-      this.edges.TryGetValue(point, out output);
-      return output;
-    }
-
-    public Vertex getVertex(Vector2 data) {
-      foreach(Vertex item in this.vertices) {
-        if (item.data == data) {
-          return item;
-        }
-      }
-      return null;
-    }
-
-    public void populateEdges() {
-      Debug.Log(this.vertices.Count);
-      foreach(Vertex item in this.vertices) {
-        foreach(Vertex compareItem in this.vertices) {
-          if (item != compareItem) {
-            float distance = Vector2.Distance(item.data, compareItem.data);
-            if (distance <= .5f) {
-              this.addEdge(item, compareItem);
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private class Vertex : IComparable<Vertex> {
-    public Vector2 data;
-    public float priority;
-
-    public Vertex(Vector2 data) {
-      this.data = data;
-    }
-
-    public int CompareTo(Vertex other) {
-      if (other == null) {
-        return -1;
-      }
-      if (this.priority == other.priority) return 0;
-      else if (this.priority > other.priority) return 1;
-      return -1;
-    }
-  }
-
-  private class PriorityQueue <T> where T : IComparable <T> {
-    private List <T> data;
-
-    public PriorityQueue()
-    {
-      this.data = new List <T>();
-    }
-
-    public void Enqueue(T item) {
-      data.Add(item);
-      int ci = data.Count - 1;
-      while (ci  > 0)
-      {
-        int pi = (ci - 1) / 2;
-        if (data[ci].CompareTo(data[pi]) >= 0)
-          break;
-        T tmp = data[ci]; data[ci] = data[pi]; data[pi] = tmp;
-        ci = pi;
-      }
-    }
-
-    public T Dequeue() {
-      // Assumes pq isn't empty
-      int li = data.Count - 1;
-      T frontItem = data[0];
-      data[0] = data[li];
-      data.RemoveAt(li);
-
-      --li;
-      int pi = 0;
-      while (true)
-      {
-        int ci = pi * 2 + 1;
-        if (ci  > li) break;
-        int rc = ci + 1;
-        if (rc  <= li && data[rc].CompareTo(data[ci])  < 0)
-          ci = rc;
-        if (data[pi].CompareTo(data[ci])  <= 0) break;
-        T tmp = data[pi]; data[pi] = data[ci]; data[ci] = tmp;
-        pi = ci;
-      }
-      return frontItem;
-    }
-
-    public int Count() {
-      return data.Count;
-    }
-  }
-
-	// private Stack<Vector2> GenerateGraph(Vector2 targetA) {
-	// 	Debug.Log("game object layer: " + Physics2D.OverlapPoint(new Vector2(2.8f, 0.1f)).gameObject.layer);
-	// 	return new Stack<Vector2>();
-	// }
-
-  private Stack<Vector2> GenerateGraph(Vector2 targetA) {
-    space = new Graph();
-    float xDist = targetA.x - transform.position.x;
-    float yDist = targetA.y - transform.position.y;
-		// float yDist = 10f;	
-    float currentX = transform.position.x;
-    float startingY = transform.position.y;
-		// space.addVertex(new Vertex(transform.position));
-    if (xDist > 0) {
-      while (targetA.x - currentX > .2f) {
-        float currentY = startingY;
-        if (yDist > 0) {
-          while (3f - currentY > .2f) {
-            Vertex point = new Vertex(new Vector2(currentX, currentY));
-						if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(point.data)) {
-							space.addVertex(point);
-						}
-            currentY = currentY + .2f;
-          }
-        } else {
-          while (3f - currentY < .2f) {
-            Vertex point = new Vertex(new Vector2(currentX, currentY));
-						// space.addVertex(point);
-						if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(point.data)) {
-							space.addVertex(point);
-						}
-            currentY = currentY - .2f;
-          }
-        }
-        currentX = currentX + .2f;
-      }
-    } else {
-      while (targetA.x - currentX < .2f) {
-        float currentY = startingY;
-        if (yDist > 0) {
-          while (3f - currentY > .2f) {
-						Vertex point = new Vertex(new Vector2(currentX, currentY));
-						// space.addVertex(point);
-						if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(point.data)) {
-							space.addVertex(point);
-						}
-            currentY = currentY + .2f;
-          }
-        } else {
-          while (3f - currentY < .2f) {
-						Vertex point = new Vertex(new Vector2(currentX, currentY));
-						// space.addVertex(point);
-						if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(point.data)) {
-							space.addVertex(point);
-						}
-            currentY = currentY - .2f;
-          }
-        }
-        currentX = currentX - .2f;
-      }
-    }
-		Vertex dest = new Vertex(targetA);
-		if (!GameObject.Find("Obstacle").GetComponent<Collider>().bounds.Contains(dest.data)) {
-			space.addVertex(dest);
-		}
-		// space.addVertex(dest);
-    space.populateEdges();
-		Debug.Log(space.getVertex(new Vector2(transform.position.x, transform.position.y)));
-  	Stack<Vector2> list = AStar(space.getVertex(new Vector2(transform.position.x, transform.position.y)), space.getVertex(targetA));
-		return list;
-  }
-
-  private Stack<Vector2> backchain(Dictionary<Vertex, Vertex> path, Vertex destination) {
-    string pathString = "";
-    pathString = pathString + destination.data + " -> ";
-    Vertex current = path[destination];
-    int count = 0;
-		Stack<Vector2> reversedPath = new Stack<Vector2>();
-		reversedPath.Push(destination.data);
-
-    while (current != null) {
-      count++;
-      pathString = pathString + current.data + " -> ";
-			reversedPath.Push(current.data);
-      current = path[current];
-    }
-    Debug.Log("path: " + pathString);
-    Debug.Log("count: " + count);
-		return reversedPath;
-  }
-
-  // AStar Seach Algorithm
-  private Stack<Vector2> AStar(Vertex start, Vertex dest) {
-
-		if (start == null || dest == null) return new Stack<Vector2>();
-
-		Debug.Log("Start: " + start.data);
-    Debug.Log("Dest: " + dest.data);
-
-    // Queue<Vertex> frontier = new Queue<Vertex>();
-    PriorityQueue<Vertex> frontier = new PriorityQueue<Vertex>();
-    start.priority = 0;
-    frontier.Enqueue(start);
-    Dictionary<Vertex, Vertex> cameFrom = new Dictionary<Vertex, Vertex>();
-    Dictionary<Vertex, float> costSoFar = new Dictionary<Vertex, float>();
-    cameFrom[start] = null;
-    costSoFar[start] = 0;
-
-    while (frontier.Count() > 0) {
-      Vertex current = frontier.Dequeue();
-      if (current.data == dest.data) {
-        Debug.Log("found the solution");
-        return backchain(cameFrom, dest);
-      }
-      foreach (Vertex next in space.getEdges(current)) {
-        float newCost = costSoFar[current] + Vector2.Distance(current.data, next.data);
-        if (!costSoFar.ContainsKey(next) || newCost < costSoFar[next]) {
-          costSoFar[next] = newCost;
-          next.priority = newCost + heuristic(dest, next);
-          frontier.Enqueue(next);
-          cameFrom[next] = current;
-        }
-      }
-    }
-    return new Stack<Vector2>();
-    
-  }
-
-  private float heuristic(Vertex a, Vertex b) {
-	return Vector2.Distance(a.data, b.data);
-  }
 }
