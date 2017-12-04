a0f7cce74e1eae3c3c730d8496f578c0b138f4f6
Josiah Putman <Josiah.K.Putman.20@Dartmouth.edu>
2017-12-04 11:12:00 -0500

    Smoothed camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,10 @@ public class CameraController : MonoBehaviour {
     public Player player;
 	public bool following = false;
     public float SCROLL_SPEED;
+    public float smoothTime;
 
     private Vector3 offset;         // offset between the player and the camera
+    private Vector3 velocity = Vector3.zero;
 
     // Use this for initialization
     void Start () 
@@ -22,6 +24,6 @@ public class CameraController : MonoBehaviour {
         
         following = !player.slashIndicator.drawing;
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        if (following) transform.position += SCROLL_SPEED * ((player.transform.position + offset) - transform.position);
+        if (following) transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref velocity, smoothTime);
     }
 }
