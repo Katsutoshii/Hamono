c47ef8af44bc159bccdea9eaa525b0a76c7605a4
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-05 15:40:32 -0800

    turns around if pumps into wall

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
   
   public float maxSpeed;
+  public float direction;
 
   public enum State {
     idle,
@@ -14,11 +15,21 @@ public class Enemy : MonoBehaviour {
     dead,
   }
 
-  void Start() {
+  public Rigidbody2D rb;
 
+  void Start() {
+    rb = gameObject.GetComponent<Rigidbody2D>();
   }
 
   void Update() {
-    
+    rb.velocity = new Vector2(direction, rb.velocity.y);
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+      if (other.name == "Obstacle") {
+        direction *= -1;
+      }
+
   }
 }
