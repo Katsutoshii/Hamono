a705d4c7ec7cb8abc6aa6f38bef1ceada58cd9fc
Josiah Putman <Josiah.K.Putman.20@Dartmouth.edu>
2017-12-09 19:36:52 -0500

    Better fix for camera

--- Assets/Scripts/Management/CameraController.cs
+++ Assets/Scripts/Management/CameraController.cs
@@ -13,8 +13,11 @@ public class CameraController : MonoBehaviour {
 
     // Use this for initialization
     void Start () 
-    {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+    {   
+        // calc the player's x y, keep the z offset
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        // Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
     }
     
@@ -25,6 +28,6 @@ public class CameraController : MonoBehaviour {
         following = !player.slashIndicator.drawing;
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         if (following) transform.position = Vector3.SmoothDamp(transform.position, 
-            player.transform.position + -10f * new Vector3(0, 0, 1), ref velocity, smoothTime);
+            player.transform.position + offset, ref velocity, smoothTime);
     }
 }
