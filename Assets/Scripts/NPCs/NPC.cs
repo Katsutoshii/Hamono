246797a9016b0e1d4e4e50c0d1ca7687c9eb3984
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-09 21:57:22 -0800

    refactored variable names

--- Assets/Scripts/NPCs/NPC.cs
+++ Assets/Scripts/NPCs/NPC.cs
@@ -7,8 +7,8 @@ public class NPC : MonoBehaviour {
 
 	private Player player;
 	
-	private GameObject NPCText;
-	public GameObject SpeechText;
+	private GameObject npcText;
+	public GameObject speechText;
 	 [TextArea(3,10)]
  	public string text;
 	
@@ -51,7 +51,7 @@ public class NPC : MonoBehaviour {
 			npcText = Instantiate(speechText);
 			npcText.transform.position = new Vector3(transform.position.x, transform.position.y + 0.8f, 0);
 		}
-		NPCTextChild = NPCText.transform.GetChild(1).gameObject.GetComponent<TextTyper>();
+		NPCTextChild = npcText.transform.GetChild(1).gameObject.GetComponent<TextTyper>();
 		NPCTextChild.NPC = gameObject.GetComponent<NPC>();
 		Debug.Log("completed speech?: " + completedSpeech);
 		if (completedSpeech) {
@@ -64,7 +64,7 @@ public class NPC : MonoBehaviour {
 		} else if (!completedSpeech && player.state != Player.State.talking) {
 			// starting converstation
 			player.state = Player.State.talking;
-			allSpeech.Add(NPCText);
+			allSpeech.Add(npcText);
 			NPCTextChild.TypeText(text);
 			completedSpeech = false;
 		} else if (!completedSpeech && player.state == Player.State.talking) {
