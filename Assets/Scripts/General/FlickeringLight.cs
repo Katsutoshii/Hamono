076e93557858f7d5be732d32a21fb68121523677
Josiah Putman <Josiah.K.Putman.20@Dartmouth.edu>
2017-12-19 00:43:41 -0500

    Added flickering lights

---
+++ Assets/Scripts/General/FlickeringLight.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour {
+    private Light light;
+    public float amplitude;
+    public float frequency;
+    public float offset;
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        light = GetComponent<Light>();
+        frequency += Random.Range(0, 1f);
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        light.intensity = amplitude * Mathf.Sin(frequency * Time.time) + offset;
+    }
+}
