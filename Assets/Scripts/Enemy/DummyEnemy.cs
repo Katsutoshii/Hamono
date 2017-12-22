a6a60e0642f60b883794cae80881fd0ce0403025
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-21 16:35:57 -0800

    addressed typo

--- Assets/Scripts/Enemy/DummyEnemy.cs
+++ Assets/Scripts/Enemy/DummyEnemy.cs
@@ -103,7 +103,7 @@ public class DummyEnemy : Enemy {
 	}
 
   protected virtual void UpdateAnimatorVariables() {
-    base.animator.SetBogol("leftHit", leftHit);
+    base.animator.SetBool("leftHit", leftHit);
     base.animator.SetBool("rightHit", rightHit);
   }
 
