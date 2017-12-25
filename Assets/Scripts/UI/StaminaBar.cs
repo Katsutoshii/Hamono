1af722ff874592a6bfcbb8e4e627130d42562751
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-24 22:25:16 -0800

    stamina changes color depending on amount

--- Assets/Scripts/UI/StaminaBar.cs
+++ Assets/Scripts/UI/StaminaBar.cs
@@ -40,10 +40,28 @@ public class StaminaBar : MonoBehaviour
       return this.exhausted;
     }
 
+    // visual representation of stamina
+    private void HandleColor() {
+      if (this.bar.fillAmount <= .3) {
+        // turn red
+        Debug.Log("turning red");
+        this.bar.color = new Color(1, 0, 0, 1);
+      } else if (this.bar.fillAmount <= .7) {
+        // turn orange
+        Debug.Log("turning orange");
+        this.bar.color = new Color(1, 0.39f, 0, 1);
+      } else {
+        // turn green
+        Debug.Log("turning green");
+        this.bar.color = new Color(1, 1, 1, 1);
+      }
+    }
+
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update() {
         fillAmount = bar.fillAmount;
+        HandleColor();
     }
 }
