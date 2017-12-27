0a469e6f41bfd1cad215c71b7e298d07e173fea1
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-26 22:36:23 -0800

    made a base class

--- Assets/Scripts/NPCs/NPC.cs
+++ Assets/Scripts/NPCs/NPC.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 using RedBlueGames.Tools.TextTyper;
 using UnityEngine.UI;
 
-public class NPC : MonoBehaviour {
-
-	private Player player;
+public class NPC : TextScript {
 	
 	private GameObject NPCMessage;
-	 [TextArea(3,10)]
- 	public string text;
-	
-	public bool completedSpeech;
-	private bool dialogStarted;
 
 	public Texture2D cursorTexture;
 	public Texture2D speechBubble;
@@ -23,19 +16,12 @@ public class NPC : MonoBehaviour {
 	void Awake() {
 		NPCMessage = transform.GetChild(0).gameObject;
 		NPCMessage.SetActive(false);
-	}
-
-	// Use this for initialization
-	void Start () {
-		
-		player = GameObject.Find("Player").GetComponent<Player>();
-		completedSpeech = false;
-
+		textOutput = NPCMessage.transform.GetChild(1).GetComponent<TextTyper>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (dialogStarted && Input.GetMouseButtonDown(0)) StartDialogue(); // continue dialog if we click anywhere
+		if (dialogStarted && Input.GetMouseButtonDown(0)) StartText(); // continue dialog if we click anywhere
 	}
 
 	/// <summary>
@@ -45,7 +31,7 @@ public class NPC : MonoBehaviour {
 	{
 		Cursor.SetCursor(speechBubble, hotSpot, cursorMode);
 		if(Input.GetMouseButtonDown(1)) {
-			StartDialogue();
+			StartText();
 		}
 	}
 
@@ -57,10 +43,11 @@ public class NPC : MonoBehaviour {
 		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
 	}
 
-	private void StartDialogue() {
-		dialogStarted = true;
-		Debug.Log("starting dialogue");
-		// triggers a speech bubble
+	protected override void ResetTextOutput() {
+		NPCMessage.SetActive(false);
+	}
+
+	protected override void SetUpText() {
 		TextTyper NPCMessageText;
 
 		// shows on screen
@@ -71,23 +58,40 @@ public class NPC : MonoBehaviour {
 		NPCMessage.transform.GetChild(2).GetComponent<Image>().sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
 		NPCMessageText.NPC = gameObject.GetComponent<NPC>();
 		Debug.Log("completed speech?: " + completedSpeech);
-
-		if (completedSpeech) {
-			// ending conversation
-			dialogStarted = false;
-			completedSpeech = false;
-			NPCMessage.SetActive(false);
-			player.state = Player.State.finishedTalking;
-		} else if (!completedSpeech && player.state != Player.State.talking) {
-			// starting converstation
-			player.ResetToIdle();
-			player.state = Player.State.talking;
-			NPCMessageText.TypeText(text);
-			completedSpeech = false;
-		} else if (!completedSpeech && player.state == Player.State.talking) {
-			NPCMessageText.Skip();
-		}
 	}
+
+	// private void StartDialogue() {
+	// 	dialogStarted = true;
+	// 	Debug.Log("starting dialogue");
+	// 	// triggers a speech bubble
+		
+	// 	TextTyper NPCMessageText;
+
+	// 	// shows on screen
+	// 	if (!NPCMessage.active) NPCMessage.SetActive(true);
+
+	// 	// gets TextTyper object
+	// 	NPCMessageText = NPCMessage.transform.GetChild(1).GetComponent<TextTyper>();
+	// 	NPCMessage.transform.GetChild(2).GetComponent<Image>().sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+	// 	NPCMessageText.NPC = gameObject.GetComponent<NPC>();
+	// 	Debug.Log("completed speech?: " + completedSpeech);
+
+	// 	if (completedSpeech) {
+	// 		// ending conversation
+	// 		dialogStarted = false;
+	// 		completedSpeech = false;
+	// 		NPCMessage.SetActive(false);
+	// 		player.state = Player.State.finishedTalking;
+	// 	} else if (!completedSpeech && player.state != Player.State.talking) {
+	// 		// starting converstation
+	// 		player.ResetToIdle();
+	// 		player.state = Player.State.talking;
+	// 		NPCMessageText.TypeText(text);
+	// 		completedSpeech = false;
+	// 	} else if (!completedSpeech && player.state == Player.State.talking) {
+	// 		NPCMessageText.Skip();
+	// 	}
+	// }
 
 	// Grabs the nearest NPC able to chat
 	// distance defines the area space that picks up NPCs
--- Assets/Scripts/UI/TextScript.cs
+++ Assets/Scripts/UI/TextScript.cs
@@ -7,7 +7,58 @@ using UnityEngine.SceneManagement;
 
 #pragma warning disable CS0219  
 
-/// Base class for enemies
+/// Base class for text scripts
 public class TextScript : MonoBehaviour {
 
+  protected Player player;
+  protected TextTyper textOutput;
+
+  [TextArea(3, 10)]
+  public string text;
+
+  public bool completedSpeech;
+	protected bool dialogStarted;
+
+  // Use this for initialization
+	void Start () {
+		
+		player = GameObject.Find("Player").GetComponent<Player>();
+		completedSpeech = false;
+
+	}
+
+
+  // handles the text at the end of the interaction
+  protected virtual void ResetTextOutput() {
+    // NPCMessage.SetActive(false);
+  }
+
+  // sets up extra prefabs and/or game objects
+  protected virtual void SetUpText() {
+
+  }
+
+  protected virtual void StartText() {
+    dialogStarted = true;
+		Debug.Log("starting dialogue");
+
+    SetUpText();
+
+		if (completedSpeech) {
+			// ending conversation
+			dialogStarted = false;
+			completedSpeech = false;
+			// NPCMessage.SetActive(false);
+      ResetTextOutput();
+			player.state = Player.State.finishedTalking;
+		} else if (!completedSpeech && player.state != Player.State.talking) {
+			// starting converstation
+			player.ResetToIdle();
+			player.state = Player.State.talking;
+			textOutput.TypeText(text);
+			completedSpeech = false;
+		} else if (!completedSpeech && player.state == Player.State.talking) {
+			textOutput.Skip();
+		}
+  }
 }
