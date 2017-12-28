d51e676386495264da78ab76fbad9e7e95c369dd
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-28 12:27:10 -0800

    created overlay script and prefab

---
+++ Assets/Scripts/UI/Overlay.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay : MonoBehaviour {
+
+  public bool toggleOverlay;
+  public float toggleFrequency;
+  private Image backdrop;
+  private Image overlayImage;
+
+  void Start() {
+    backdrop = GameObject.Find("Backdrop").GetComponent<Image>();
+    overlayImage = GameObject.Find("OverlayImage").GetComponent<Image>();
+    if (toggleOverlay) StartCoroutine(ToggleImage());
+  }
+
+  private IEnumerator ToggleImage() {
+    int toggleCount = 0;
+    while (toggleCount < 7) {
+      yield return new WaitForSeconds(toggleFrequency);
+      overlayImage.color = new Color(1, 1, 1, overlayImage.color.a * -1);
+      toggleCount++;
+    }
+    StartCoroutine(FadeOut());
+  }
+
+  private IEnumerator FadeOut() {
+    while (backdrop.color.a > 0) {
+      yield return new WaitForSeconds(.2f);
+      backdrop.color = new Color(backdrop.color.r, backdrop.color.g, backdrop.color.b, backdrop.color.a - .1f);
+    }
+    gameObject.SetActive(false);
+  }
+}
