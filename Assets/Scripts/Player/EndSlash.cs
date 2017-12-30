daa6a1d60bf84c7792ac53791700321b3da6b3b8
Josiah Putman <Josiah.K.Putman.20@Dartmouth.edu>
2017-12-30 11:34:38 -0500

    Working on level 2

--- Assets/Scripts/Player/EndSlash.cs
+++ Assets/Scripts/Player/EndSlash.cs
@@ -17,6 +17,7 @@ public class EndSlash : StateMachineBehaviour {
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		animator.gameObject.GetComponent<Player>().EndSlash();
 		animator.SetBool(slashName, false);
 	}
 
