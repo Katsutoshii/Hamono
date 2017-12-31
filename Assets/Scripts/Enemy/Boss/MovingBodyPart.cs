ccba9f30ff4d8bec9a618cfe78060cfc6cc96c25
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-31 13:54:18 -0800

    included assets. made sprites sheets for half exploding hands

---
+++ Assets/Scripts/Enemy/Boss/MovingBodyPart.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#pragma warning disable CS0219  
+
+/// Base class for boss body parts
+public class MovingBodyPart : MonoBehaviour {
+
+  void Start() {}
+
+  void Update() {}
+  
+}
