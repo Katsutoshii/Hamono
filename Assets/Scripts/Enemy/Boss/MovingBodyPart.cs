1385a9fe608b921951d6a6b3f7d5281d9ea6a58c
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-31 15:09:24 -0800

    rotational movement for boss

--- Assets/Scripts/Enemy/Boss/MovingBodyPart.cs
+++ Assets/Scripts/Enemy/Boss/MovingBodyPart.cs
@@ -8,8 +8,27 @@ using UnityEngine.UI;
 /// Base class for boss body parts
 public class MovingBodyPart : MonoBehaviour {
 
+  public float rotationalSpeed;
+  public bool clockwise;
+
+  private float angle = 0;
+  private float speed; // 2 * PI in degrees is 360, so you get 5 seconds to complete a circle
+  float radius = .2f;
+
+  void Awake() {
+    speed = (2 * Mathf.PI) / rotationalSpeed;
+  }
+
   void Start() {}
 
-  void Update() {}
-  
+  void Update() {
+    Move();
+  }
+
+  private void Move() {
+    if (clockwise) angle += speed * Time.deltaTime; //if you want to switch direction, use -= instead of +=
+    else angle -= speed * Time.deltaTime;
+    transform.position = new Vector2(Mathf.Cos(angle) * radius, 2.7f - Mathf.Sin(angle) * radius);
+  }
+
 }
