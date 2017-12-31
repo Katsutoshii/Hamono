10f29d9e7c6e56aaa17f3ecaa7f8c89f4f0defdc
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-31 15:13:00 -0800

    entire boss's body moves

--- Assets/Scripts/Enemy/Boss/MovingBodyPart.cs
+++ Assets/Scripts/Enemy/Boss/MovingBodyPart.cs
@@ -15,8 +15,13 @@ public class MovingBodyPart : MonoBehaviour {
   private float speed; // 2 * PI in degrees is 360, so you get 5 seconds to complete a circle
   float radius = .2f;
 
+  private float originalX;
+  private float originalY;
+
   void Awake() {
     speed = (2 * Mathf.PI) / rotationalSpeed;
+    originalX = transform.position.x;
+    originalY = transform.position.y;
   }
 
   void Start() {}
@@ -28,7 +33,7 @@ public class MovingBodyPart : MonoBehaviour {
   private void Move() {
     if (clockwise) angle += speed * Time.deltaTime; //if you want to switch direction, use -= instead of +=
     else angle -= speed * Time.deltaTime;
-    transform.position = new Vector2(Mathf.Cos(angle) * radius, 2.7f - Mathf.Sin(angle) * radius);
+    transform.position = new Vector2(originalX + Mathf.Cos(angle) * radius, originalY + Mathf.Sin(angle) * radius);
   }
 
 }
