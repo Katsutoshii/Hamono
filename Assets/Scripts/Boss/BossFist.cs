1abcdcdc7147a5420225662b418de84b42205554
Josiah Putman <Josiah.K.Putman.20@Dartmouth.edu>
2018-01-01 11:39:22 -0500

    Boss correctly moves up

--- Assets/Scripts/Boss/BossFist.cs
+++ Assets/Scripts/Boss/BossFist.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossEnemy : MonoBehaviour {
+public class BossFist : MonoBehaviour {
 
+	private Boss boss;
 	// Use this for initialization
 	void Start () {
-		
+		boss = GetComponentInParent<Boss>();
 	}
 	
 	// Update is called once per frame
