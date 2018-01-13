d37553b264532e158001f7668b42111e061fd942
ijemmao <ijemmaonwuzulike10@gmail.com>
2018-01-13 15:43:29 -0500

    i fixed them....rise dummies, rise

--- Assets/Scripts/Enemy/DummyEnemy.cs
+++ Assets/Scripts/Enemy/DummyEnemy.cs
@@ -20,79 +20,10 @@ public class DummyEnemy : Enemy {
 
   public override void Damage(float damageAmount, float knockback, Collider2D source) {
     base.state = State.damaged;
-
-    HandleAttack(source);
-  }
-  
-  private void HandleAttack(Collider2D source) {
-    if (source.transform.position.x < transform.position.x) {
-        switch(typeOfBot) {
-          case "normal":
-            leftHit = true;
-            base.animator.SetBool("leftHit", leftHit);
-            PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
-            break;
-
-          case "head":
-            if (player.attackType == Player.AttackType.upSlash) {
-              leftHit = true;
-              base.animator.SetBool("leftHit", leftHit);
-              PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
-            }
-            break;
-
-          case "body":
-            if (player.attackType == Player.AttackType.straightSlash) {
-              leftHit = true;
-              base.animator.SetBool("leftHit", leftHit);
-              PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
-            }
-            break;
-
-          case "legs":
-            if (player.attackType == Player.AttackType.downSlash) {
-              leftHit = true;
-              base.animator.SetBool("leftHit", leftHit);
-              PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
-            }
-            break;
-        }
-
-    }
-    else {
-      switch(typeOfBot) {
-          case "normal":
-            rightHit = true;
-            base.animator.SetBool("rightHit", rightHit);
-            PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
-            break;
-
-          case "head":
-            if (player.attackType == Player.AttackType.upSlash) {
-              rightHit = true;
-              base.animator.SetBool("rightHit", rightHit);
-              PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
-            }
-            break;
-
-          case "body":
-            if (player.attackType == Player.AttackType.straightSlash) {
-              rightHit = true;
-              base.animator.SetBool("rightHit", rightHit);
-              PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
-            }
-            break;
-
-          case "legs":
-            if (player.attackType == Player.AttackType.downSlash) {
-              rightHit = true;
-              base.animator.SetBool("rightHit", rightHit);
-              PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
-            }
-            break;
-        }
-    }
-    StartCoroutine(ResetHit());
+    if (source.transform.position.x < transform.position.x) leftHit = true;
+    else rightHit = true;
+    PoolManager.instance.ReuseObject(coinPrefab, transform.position, transform.rotation, coinPrefab.transform.localScale);
+    StartCoroutine(ResetHit()); // stops animation loop
   }
 
   private IEnumerator ResetHit() {
