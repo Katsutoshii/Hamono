a6016eecc07192124baa0671967dec0f6b5e64ba
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-09-08 14:25:06 -0400

    changed the name of zip file

--- Website/index.js
+++ Website/index.js
@@ -8,7 +8,7 @@ window.onload = () => {
   const downloadButton = document.getElementById('download-game')
   const downloadbuttonLarge = document.getElementById('download-game-large')
   if (OSName === 'Unknown OS') { downloadButton.href = '#'; downloadButton.innerText = 'not available'; downloadbuttonLarge.innerText = 'not available' }
-  if (OSName !== 'Windows') { downloadButton.href = 'game/Hamono_Mac.zip'; downloadbuttonLarge.href = 'game/Hamono_Mac.zip' }
+  if (OSName !== 'Windows') { downloadButton.href = './game/Hamono_Mac.zip'; downloadbuttonLarge.href = './game/Hamono_Mac.zip' }
   // if (OSName === 'Windows') { downloadButton.href = 'game/Hamono.exe'; downloadbuttonLarge.href = 'game/Hamono.zip' }
   if (OSName === 'Windows') { downloadButton.href = '#'; downloadButton.innerText = 'coming soon for windows'; downloadbuttonLarge.innerText = 'coming soon for windows' }
 
